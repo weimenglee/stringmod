9aee457d9000a27235806d7e2e34c64073e83668
Wei-Meng Lee <weimenglee@learn2develop.net>
2021-01-31 00:29:39 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/weimenglee/stringmod
+
+go 1.15
+
+require github.com/hackebrot/turtle v0.1.0
---
+++ strings/strings.go
@@ -0,0 +1,20 @@
+package strings
+
+func internalFunction() {
+	// In Go, a name is exported if it
+	// begins with a capital letter
+}
+
+// Must begin with a capital letter in
+// order to be exported
+func CountOddEven(s string) (odds, evens int) {
+	odds, evens = 0, 0
+	for _, c := range s {
+		if int(c)%2 == 0 {
+			evens++
+		} else {
+			odds++
+		}
+	}
+	return
+}
